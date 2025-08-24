9a7231b68bf70d5f2e02aca0738618363b825050
Brayden Adcox <braydenadcox@gmail.com>
2025-08-23 19:14:45 -0700

    Added links to social media platforms

--- bryxzn/src/app/page.tsx
+++ bryxzn/src/app/page.tsx
@@ -7,17 +7,17 @@ export default function Home() {
           url('/images/BryxznTexture.png'),   /* texture layer */
           linear-gradient(#303030, #1f1f1f) /* gradient layer */
         `,
-        backgroundSize: "cover, cover", // match both layers
+        backgroundSize: "cover, cover", 
         backgroundPosition: "center, center",
-        backgroundRepeat: "repeat, no-repeat", // noise repeats, gradient doesn't
-        backgroundBlendMode: "overlay", // blend texture into gradient
+        backgroundRepeat: "repeat, no-repeat", 
+        backgroundBlendMode: "overlay", 
         position: "relative",
       }}
     >
       <h1
         style={{
           position: "absolute",
-          top: "20%",
+          top: "18%",
           left: "50%",
           transform: "translate(-50%, -50%)",
           textAlign: "center",
@@ -49,6 +49,19 @@ export default function Home() {
           >
             Welcome to my page!
           </p>
+          <div style={{
+            display: "flex",
+            justifyContent: "center",
+            fontSize: "1.6rem",
+            marginTop: "1.5rem",
+            fontWeight: 500,
+            color: "#ffffff",
+            gap: "1.5rem",
+          }}>
+            <a href='https://www.instagram.com/bryxzn.tech/' target="_blank" rel="noopener noreferrer">Instagram</a>
+            <a href='https://www.tiktok.com/@bryxzn.tech' target="_blank" rel="noopener noreferrer">TikTok</a>
+            <a href='https://www.youtube.com/@bryxznn' target="_blank" rel="noopener noreferrer">YouTube</a>
+          </div>
         </span>
       </h1>
     </main>
